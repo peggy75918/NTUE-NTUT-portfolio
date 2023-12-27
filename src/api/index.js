87abbe22aece64c76fc7846bae7294efc00dd582
Peggy <peggy4328@gmail.com>
2023-12-27 15:39:19 +0800

    chang api

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,10 @@
 import axios from "axios";
 
-const baseUrl =
-  "https://script.google.com/macros/s/AKfycbzUvJmNkD6ho5dgCKL5gTLE9pcZc8wXhuxsAE5Uy17OxOBSxoZuPDC2tgdcShzRFr1g7w/exec";
+const URL = "https://fastapi-for-f2eworks.vercel.app/homeworks/";
 
 export const getSheetData = async () => {
   try {
-
-    const result = await axios.get(`${baseUrl}`);
-
+    const result = await axios.get(URL);
     return result.data;
   } catch (err) {
     console.log(err);
